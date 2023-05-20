dbe1d3e5c2bed7c4ff000ec9c6f02a918362c6f2
angversh <aavershinina_1@edu.hse.ru>
2023-05-20 11:42:37 +0300

    Add files via upload

---
+++ 20_Calculator.cpp
@@ -0,0 +1,266 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <limits>
+
+#include "20_List.cpp"
+
+using namespace std;
+
+int input() {  //ввод с консоли, если не выход 
+	long long input;
+	while (1) {
+		cin >> input;
+		if (cin.fail() || cin.peek() != '\n' || abs(input) > std::numeric_limits<int>::max()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		else {
+			break;
+		}
+	}
+	return int(input);
+}class Strings {
+
+public:
+	string s1(string s0) {
+		string out = "";
+		string ERR = "";
+		if (s0.size() == 0) {
+			return ERR;
+		}
+		if (!isspace(s0[0])) {
+			if (isdigit(s0[0])) {
+				if (s0.size() > 1 && (s0[1] == '+' || s0[1] == '-' || s0[1] == '*' || s0[1] == '/')) {
+					return ERR;
+				}
+				out += s0[0];
+			}
+			else {
+				return ERR;
+			}
+		}
+		if (s0.size() == 1) {
+			return out;
+		}
+		else {
+			for (int i = 1; i < s0.size(); i++) {
+				if (isdigit(s0[i]) || s0[i] == '+' || s0[i] == '-' || s0[i] == '*' || s0[i] == '/') {
+					out += s0[i];
+					if (isdigit(s0[i]) && i < s0.size() - 1 && (s0[i + 1] == '+' || s0[i + 1] == '-' || s0[i + 1] == '*' || s0[i + 1] == '/')) {
+						out += ' ';
+					}
+					if ((s0[i] == '+' || s0[i] == '-' || s0[i] == '*' || s0[i] == '/') && i < s0.size() - 1 && (isdigit(s0[i + 1]) || s0[i + 1] == '+' || s0[i + 1] == '-' || s0[i + 1] == '*' || s0[i + 1] == '/')) {
+						out += ' ';
+					}
+				}
+				else if (isspace(s0[i])) {
+					if (isdigit(s0[i - 1]) || s0[i - 1] == '+' || s0[i - 1] == '-' || s0[i - 1] == '*' || s0[i - 1] == '/') {
+						out += ' ';
+					}
+				}
+				else {
+					return ERR;
+				}
+			}
+			if (out.size() == 0) {
+				return ERR;
+			}
+			if (out[out.size() - 1] == ' ') {
+				out.erase(out.size() - 1);
+			}
+			return out;
+		}
+	}
+};
+class Operations {
+
+public:
+	double res;
+	int ErrorCode;
+
+	Operations() {
+		this->res = 0;
+		this->ErrorCode = 0;
+	}
+
+	void MakeOp(double a, double b, char oper) {
+		switch (oper) {
+		case '+':
+			if ((b > 0 && a > std::numeric_limits<int>::max() - b) || (b < 0 && a < std::numeric_limits<int>::min() - b)) {
+				this->ErrorCode = 1;
+			}
+			else {
+				this->res = a + b;  //sum
+			}
+			break;
+		case '-':
+			if ((b < 0 && a > std::numeric_limits<int>::max() + b) || (b > 0 && a < std::numeric_limits<int>::min() + b)) {
+				this->ErrorCode = 1;
+			}
+			else {
+				this->res = a - b;  //subtraction
+			}
+			break;
+		case '*':
+			if (a == 0 || b == 0) {
+				this->res = 0;
+			}
+			else if ((b > 0) && ((a > std::numeric_limits<int>::max() / b) || (a < std::numeric_limits<int>::min() / b))) {
+				this->ErrorCode = 1;
+			}
+			else if ((b < 0) && ((a < std::numeric_limits<int>::max() / b) || (a > std::numeric_limits<int>::min() / b))) {
+				this->ErrorCode = 1;
+			}
+			else {
+				this->res = a * b;   //multiplication
+			}
+			break;
+		case '/':
+			if (b == 0) {
+				this->ErrorCode = 2;
+			}
+			else if ((b > 0) && ((a > std::numeric_limits<int>::max() * b) || (a < std::numeric_limits<int>::min() * b))) {
+				this->ErrorCode = 1;
+			}
+			else if ((b < 0) && ((a < std::numeric_limits<int>::max() * b) || (a > std::numeric_limits<int>::min() * b))) {
+				this->ErrorCode = 1;
+			}
+			else {
+				this->res = a / b;  //division
+			}
+			break;
+		}
+	}
+	void Clear() {
+		this->res = 0;
+		this->ErrorCode = 0;
+	}
+};
+
+int main() {
+	20_List Stack;
+
+	Operations Op;
+	Strings Q;
+	string expr;
+
+	int inp;
+	double num1, num2, num = 0;
+	bool flag = true;
+	bool err = false;
+
+	cout << "Enter the operation!" << endl;
+	cout << "1) Count the expression" << endl;
+	cout << "2) Exit" << endl;
+
+	while (1) {
+		inp = input();
+		switch (inp) {
+		case 1:
+			Stack.DeleteList();
+			Op.Clear();
+			err = false;
+			num = 0;
+			cout << "Enter the expression:" << endl;
+			cin.ignore();
+			getline(cin, expr);
+			expr = Q.s1(expr);
+			if (expr.size() == 0) {
+				cout << "Incorrect expression!" << endl;
+				break;
+			}
+			cout << "You entered: " << expr << endl;
+			for (int j = 0; j < expr.size(); j++) {
+				if (isdigit(expr[j])) {
+					if (num <= std::numeric_limits<int>::max() / 10) {
+						num *= 10;
+					}
+					else {
+						cout << "Overflow!" << endl;
+						err = true;
+						break;
+					}
+					if (num <= std::numeric_limits<int>::max() - (expr[j] - '0')) {
+						num += (expr[j] - '0');
+					}
+					else {
+						cout << "Over flow!" << endl;
+						err = true;
+						break;
+					}
+					flag = true;
+					if (j == expr.size() - 1) {
+						Stack.AddEnd(num, Stack.HeadNode);
+						num = 0;
+					}
+				}
+				else if ((expr[j] == '+') || (expr[j] == '-') || (expr[j] == '*') || (expr[j] == '/')) {
+					if (Stack.IsEmpty()) {
+						cout << "Incorrect expression!" << endl;
+						err = true;
+						break;
+					}
+					else {
+						num2 = Stack.FindPosition(Stack.ListSize() - 1);
+						Stack.DeletePosition(Stack.ListSize() - 1, Stack.HeadNode);
+					}
+					if (Stack.IsEmpty()) {
+						cout << "Incorrect expression!" << endl;
+						err = true;
+						break;
+					}
+					else {
+						num1 = Stack.FindPosition(Stack.ListSize() - 1);
+						Stack.DeletePosition(Stack.ListSize() - 1, Stack.HeadNode);
+					}
+					Op.MakeOp(num1, num2, expr[j]);
+					if (Op.ErrorCode == 0) {
+						Stack.AddEnd(Op.res, Stack.HeadNode);
+						flag = false;
+						Op.Clear();
+					}
+					else if (Op.ErrorCode == 1) {
+						cout << "Over flow!" << endl;
+						err = true;
+						Op.Clear();
+						break;
+					}
+					else {
+						cout << "Unable to divide by zero!" << endl;
+						err = true;
+						Op.Clear();
+						break;
+					}
+				}
+				else {
+					if (flag) {
+						Stack.AddEnd(num, Stack.HeadNode);
+						num = 0;
+					}
+				}
+			}
+			if (err) {
+				break;
+			}
+			if (Stack.ListSize() != 1) {
+				cout << "The expression is incorrect!" << endl;
+				break;
+			}
+			if (Stack.FindPosition(0) == static_cast<int>(Stack.FindPosition(0))) {
+				cout << "Answer is: " << fixed << static_cast<int>(Stack.FindPosition(0)) << endl;
+			}
+			else {
+				cout << "Answer is: " << defaultfloat << Stack.FindPosition(0) << endl;
+			}
+			break;
+		case 2:
+			return 0;
+		default:
+			cout << "Enter another command!" << endl;
+			break;
+		}
+	}
+	return 0;
+}
---
+++ 20_List.cpp
@@ -0,0 +1,297 @@
+﻿#include <iostream>
+#include <limits> 
+
+using namespace std;
+
+class Node {    // узел 
+public:
+    int data;
+    Node* next; // параметры узла
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }   // конструктор узла
+};
+class ListL {  // односвязный список
+public:
+    Node* HeadNode; // начало списка
+    ListL() {
+        HeadNode = NULL;
+    }   // конструктор списка
+    ~ListL() {
+        while (HeadNode != NULL) {
+            DeletePosition(0, HeadNode);
+        }
+    }   // деструктор списка
+    int IsEmpty() {
+        if (HeadNode == NULL) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }   
+
+int input() {   
+    int input;
+    while (1) {
+        cin >> input;
+        if (cin.fail() || cin.peek() != '\n') { 
+            cin.clear();    
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');    
+        }
+        else {
+            break;  
+        }
+    }
+    return input;
+}
+    void AddEnd(int a, Node*& HeadNode) {   
+        if (IsEmpty()) {
+            Node* node = new Node(a);
+            HeadNode = node;
+        }   
+        else {  
+            Node* node = new Node(a);   
+            Node* tmp = HeadNode;       
+            while (1) {
+                if ((*tmp).next != NULL) {
+                    tmp = (*tmp).next;
+                }
+                else {
+                    (*tmp).next = node;
+                    break;
+                }
+            }   
+        }
+    }
+    void AddBegin(int a, Node*& HeadNode) { 
+        if (IsEmpty()) {
+            Node* node = new Node(a);
+            HeadNode = node;
+        }   
+        else {
+            Node* node = new Node(a);
+            node->next = HeadNode;
+            HeadNode = node;
+        }   
+    }
+    void AddPosition(int a, int pos) {  
+        if (pos == 0) {
+            AddBegin(a, HeadNode);
+        }   
+        if (pos < 0) {
+            return;
+        }  
+        if (pos > 0) {
+            if (IsEmpty()) {
+                return;
+            }   
+            else {
+                Node* tmp = HeadNode;
+                int iter = -1;     
+                while (iter < pos - 2) {
+                    if ((*tmp).next == NULL) {
+                        return;     
+                    }
+                    iter++;
+                    tmp = (*tmp).next;  
+                }
+                if ((*tmp).next == NULL) {
+                    AddEnd(a, HeadNode);
+                    return;     
+                }
+                else {
+                    Node* node = new Node(a);
+                    node->next = (*tmp).next;
+                    (*tmp).next = node;
+                }   // добавление внутрь списка
+            }
+        }
+    }
+    void DeletePosition(int pos, Node*& HeadN) { 
+        if ((pos < 0) || IsEmpty()) {
+            return;
+        }   
+        if (pos == 0) {
+            Node* tmp = (*HeadN).next;
+            delete HeadN;
+            HeadN = tmp;
+        }   
+        if (pos > 0) {
+            Node* tmp = HeadN;   
+            int iter = -1;      
+            while (iter < pos - 2) {
+                if ((*tmp).next == NULL) {
+                    return;
+                }   /
+                iter++;
+                tmp = (*tmp).next;  
+            }
+            if ((*tmp).next == NULL) {
+                return;
+            }   
+            else {
+                Node* tmp1 = (*((*tmp).next)).next;
+                delete (*tmp).next;
+                (*tmp).next = tmp1;
+            }   
+        }
+    }
+    void DeleteValue(int value, Node*& HeadNode) {  
+        if (IsEmpty()) {
+            return;
+        }   
+        Node* tmp = HeadNode;   
+        int iter = -1;          
+        int deleted = 0;        
+        while (tmp != NULL) {   
+            if ((*tmp).data == value) { 
+                tmp = (*tmp).next;
+                DeletePosition(iter + 1 - deleted, HeadNode);   
+                deleted++;
+                iter++;     
+            }
+            else {
+                tmp = (*tmp).next;
+                iter++;
+            }       
+        }
+    }
+    void FindValue(int value) {     
+        if (IsEmpty()) {
+            cout << "EmptyList" << endl;
+        }       
+        else {
+            cout << "Indexes of value " << value << ": ";
+            Node* tmp = HeadNode;       
+            int iter = -1;
+            int finder = 0;
+            while (tmp != NULL) {   
+                if ((*tmp).data == value) {
+                    cout << iter + 1 << " ";
+                    finder++;
+                    iter++;
+                    tmp = (*tmp).next;
+                }       
+                else {
+                    iter++;
+                    tmp = (*tmp).next;
+                }       
+            }
+            if (finder == 0) {
+                cout << "None!";
+            }       
+            cout << endl;
+        }
+    }
+    void FindPosition(int pos) {    
+        if (IsEmpty()) {
+            cout << "Empty List" << endl;
+            return;
+        }   
+        if (pos < 0) {
+            cout << "Incorrect Position!" << endl;
+            return;
+        }   
+        Node* tmp = HeadNode;
+        int iter = -1;
+        int finded = 0;
+        while (tmp != NULL) {   
+            if (iter + 1 != pos) {
+                iter++;
+                tmp = (*tmp).next;
+            }   
+            else {
+                cout << "Element on " << pos << " position is: " << (*tmp).data << endl;
+                finded++;
+                break;
+            }   
+        }
+        if (finded == 0) {
+            cout << "This position does npt exist!" << endl;
+        }   
+    }
+    void PrintList() {
+        if (IsEmpty()) {
+            cout << "Empty List" << endl;
+        }
+        else {
+            Node* tmp = HeadNode;
+            while (tmp != NULL) {
+                cout << (*tmp).data << " ";
+                tmp = (*tmp).next;
+            }
+            cout << endl;
+        }
+    }       
+};
+
+int main()
+{
+    ListL list;    //новый список
+    cout << "List Operations" << endl;
+
+    cout << "1. Add Element in the end" << endl;
+    cout << "2. Add Element in the beginning" << endl;
+    cout << "3. Add Element in position" << endl;
+    cout << "4. Delete Element of some value" << endl;
+    cout << "5. Delete Element in some position" << endl;
+    cout << "6. Find Element of some value" << endl;
+    cout << "7. Find Element in some position" << endl;
+    cout << "8. Print List" << endl;
+    cout << "9. Exit" << endl;
+
+    int inp, elem, pos, value;
+    while (1) {         
+        inp = input();
+        switch (inp) {
+        case 1:
+            cout << "Element:" << endl;
+            elem = input();
+            list.AddEnd(elem, list.HeadNode);
+            break;
+        case 2:
+            cout << "Element: " << endl;
+            elem = input();
+            list.AddBegin(elem, list.HeadNode);
+            break;
+        case 3:
+            cout << "Element: " << endl;
+            elem = input();
+            cout << "Position: " << endl;
+            pos = input();
+            list.AddPosition(elem, pos);
+            break;
+        case 4:
+            cout << "Value: " << endl;
+            value = input();
+            list.DeleteValue(value, list.HeadNode);
+            break;
+        case 5:
+            cout << "Position: " << endl;
+            pos = input();
+            list.DeletePosition(pos, list.HeadNode);
+            break;
+        case 6:
+            cout << "Value: " << endl;
+            value = input();
+            list.FindValue(value);
+            break;
+        case 7:
+            cout << "Position: " << endl;
+            pos = input();
+            list.FindPosition(pos);
+            break;
+        case 8:
+            list.PrintList();
+            break;
+        case 9:
+            return 0;
+        default:
+            cout << "Incorrect command!" << endl;
+            break;
+        }
+    }
+}
+
